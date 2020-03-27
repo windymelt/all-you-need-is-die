06a5cba41a4acbaac3f7284a771fc3aa72434239
Windymelt <windymelt@hatena.ne.jp>
2020-03-27 17:11:01 +0900

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::process::Command;
+use std::env;
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    // get args
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+ 
+    //execute command
+    let mut cmd = Command::new(&args[1]);
+    cmd.args(&args[2..]);
+    let mut output = cmd.output();
+    while let Ok(o) = output {
+        println!("status: {}", o.status);
+        io::stdout().write_all(&o.stdout).unwrap();
+        io::stderr().write_all(&o.stderr).unwrap();
+        if o.status.code() != Some(0) { break; }
+        output = cmd.output();
+    }
+
+    println!("dead!");
 }
